a2dd45e2c724e42158dfe441e1111f69f39ae44b
Jos Verlinde <jos_verlinde@hotmail.com>
2023-11-10 20:57:47 +0100

    failed tries to get decorator typing working
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

---
+++ src/decorators_typing/deco_1.py
@@ -0,0 +1,28 @@
+
+class PIOASMEmit:
+    def __init__(self) -> None:
+        self.labels = {}
+        self.prog = []
+        self.wrap_used = False
+        self.pins = 0
+    def nop(self) -> None:
+        self.prog.append(0)
+    
+
+
+def jos_pio(**kw):
+    def nop(self) -> None: ...
+    globals["nop"] = nop
+    emit = PIOASMEmit(**kw)
+    def decorator(f):
+        def nop(self) -> None: ...
+        f(emit)
+        return emit
+    return decorator
+
+@jos_pio(set_init=0)
+def blink_1hz():
+    # Cycles: 1 + 1 + 6 + 32 * (30 + 1) = 1000
+    irq(rel(0))
+    nop()
+    
---
+++ src/decorators_typing/deco_2.py
@@ -0,0 +1,42 @@
+
+class PIOASMEmit:
+    def __init__(self) -> None:
+        self.labels = {}
+        self.prog = []
+        self.wrap_used = False
+        self.pins = 0
+    def nop(self) -> None:
+        self.prog.append(0)
+    
+
+
+def pio_asm_nop() -> None: 
+    print("NOP")
+
+def jos_pio_old(**kw):
+
+    emit = PIOASMEmit(**kw)
+    def decorator(f):
+        def nop(self) -> None: ...
+        f(emit)
+        return emit
+    return decorator
+
+def jos_pio(set_init=3):
+    def wrap(f):
+        print ("Inside wrap()")
+        def wrapped_f(*args):
+            print ("Inside wrapped_f()")
+            print ("Decorator arguments:", set_init)
+            f.__globals__["nop"] = pio_asm_nop
+            f(*args)
+            print ("After f(*args)")
+        return wrapped_f
+    return wrap
+
+@jos_pio(set_init=0)
+def blink_1hz():
+    # Cycles: 1 + 1 + 6 + 32 * (30 + 1) = 1000
+    # irq(rel(0))
+    nop()
+    
